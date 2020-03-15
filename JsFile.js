a4dd9d788541661b1afa1a8ce32819aac50b016d
jadyel <jadyelbatera@gmail.com>
2020-03-15 11:33:44 -0300

    Adicionando validações com JS

--- JsFile.js
+++ JsFile.js
@@ -1,10 +1,70 @@
-
+// Habilita/desabilita campo "Quais:"
 function mostra(id){
-	if (document.getElementById(id).style.display=='block') {
-	document.getElementById(id).style.display='none';
+	document.getElementById(id).style.display='block' 
+	
+
 }
+
+function oculta(id){
+	document.getElementById(id).style.display='none' 
+}
+
+	
+// Validando campos
+
+function validacao(){
+	var formulario = document.forms["formulario"]
+	var nome = formulario.nome.value
+	var telefone = formulario.telefone.value
+
+	var  erro = false;
+
+	
+	if (nome == "") {
+		alert("Preencha o campo Nome")
+		formulario.nome.focus()
+		erro = true;
+	}
+		else{
+			if (nome.indexOf(" ")  == -1) {
+			alert("Escreva pelo menos um sobrenome!")
+			formulario.nome.focus()
+			
+			erro = true;
+			}
+		}
+
+	if (telefone == "") {
+		alert("Preencha o campo Telefone")
+		formulario.telefone.focus()
+		erro = true;
+	}
 	else{
-		document.getElementById(id).style.display='block';
+		if (telefone.length  != 10) {
+		alert("Telefone inválido!")
+		formulario.telefone.focus()
+		erro = true;
 	}
 }
-	
+
+	if (formulario.selecione[0].checked==false && formulario.selecione[1].checked==false) {
+		alert("Preencha o campo Rede social");
+		formulario.selecione[0].focus()
+		erro = true;
+	}
+      if (formulario.selecione[0].checked==true) {
+		if (formulario.redes[0].checked==false &&
+			formulario.redes[1].checked==false &&
+			formulario.redes[2].checked==false){
+			alert("Selecione pelo menos um rede social");
+			return false;
+
+        }
+    }
+		
+	if (erro) {
+		return false
+	}
+	else
+		return true
+}	
