e4c75a27eb0329376375e593d902243a1aebe24d
jadyel <jadyelbatera@gmail.com>
2020-03-16 02:24:33 -0300

    Enviando os dados para local.php

--- local.php
+++ local.php
@@ -1,5 +1,20 @@
+
 <?php
+//recebendo os dados 
+    if (isset($_POST["enviar"])){
 
-
+    $nome = $_POST["nome"];
+    $telefone= $_POST["telefone"];
+    $combobox= $_POST["combobox"];
+    $selecione= $_POST["selecione"];
+    
+// os dados de checkbox só são enviados se o campo radio estiver selecionado como sim
+}
+    if($selecione=='sim'){
+ 	foreach ($_POST["redes"] as $key => $value) {
+    echo '<br />' . $key . ' => ' . $value;
+}
+}
+	echo "<p>Olá, ".$nome."</p>"; echo "<p>seus dados foram enviados com sucesso</p>";
 
 ?>
